130101218cd23315ac48ac38da59ad9b249a6a51
HemantDutta <hemantnorddutta2@gmail.com>
2023-01-24 15:35:43 +0530

    pre modal

--- infinity/src/Components/Grid.js
+++ infinity/src/Components/Grid.js
@@ -10,6 +10,21 @@ export const Grid = () => {
 
     const client = createClient('ZtDkSHkkb20vljXydzL79IG48JZ3zUiXiSLlSXH7xut6Jqrpzhg2rOTQ');
 
+    const openModal = async (event) => {
+        console.log();
+        let id = event.currentTarget.id;
+        await client.photos.show({id})
+            .then(res => {
+                console.log(res);
+                document.getElementById('modalTitle').innerText = `Clicked by: ${res.photographer}`;
+                document.getElementById('modalImg').src = res.src.original;
+                document.getElementById('modal').click();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
    const getContent = async () => {
 
        await client.photos.curated({per_page: 32, page: pageNo})
@@ -25,6 +40,8 @@ export const Grid = () => {
             })
     }
 
+
+
     const handleScroll = async () => {
       try{
         if(window.innerHeight + document.documentElement.scrollTop + 5 > document.documentElement.scrollHeight){
@@ -38,10 +55,12 @@ export const Grid = () => {
 
     useEffect(()=>{
         getContent();
+        console.log('get content effect');
     }, [pageNo]);
 
    useEffect(()=>{
        window.addEventListener("scroll", handleScroll);
+       console.log('scroll effect');
    },[]);
 
 
@@ -57,12 +76,12 @@ export const Grid = () => {
                         data.map((value, index) => {
                             return (
                                 <>
-                                    <div className="grid-item">
+                                    <div className="grid-item" id={value.id} onClick={openModal}>
                                         <div className="card">
-                                            <div className="img-box"><img src={value.src.portrait} id={value.id} alt={value.photographer} title={value.photographer}/></div>
+                                            <div className="img-box"><img src={value.src.portrait} alt={value.photographer} title={value.photographer}/></div>
                                         </div>
                                         <div className="card-body">
-                                            <h5 className="card-title text-light fw-bold" key={index}>By: {value.photographer}</h5>
+                                            <h5 className="card-title text-light fw-bold">By: {value.photographer}</h5>
                                         </div>
                                     </div>
                                 </>
@@ -70,8 +89,34 @@ export const Grid = () => {
                         })
                     }
                 </div>
-                <div className="text-center"><img src="images/loader.gif" alt="loading..."/></div>
+                {load && `<div className="text-center"><img src="images/loader.gif" alt="loading..."/></div>`}
             </div>
+        {/*Modal*/}
+            <div>
+                {/* Button trigger modal */}
+                <button type="button" className="btn btn-primary d-none" id="modal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                </button>
+                {/* Modal */}
+                <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-xl">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="modalTitle"></h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+                            </div>
+                            <div className="modal-body modalImg">
+                                <img src="" alt="" id="modalImg"/>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Download</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        {/*Modal End*/}
         </>
     )
 }
